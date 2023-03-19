6f74a31545951b4ea149767f9875abddd1ca73e0
U-ma-s <yuma.prodia4989@gmail.com>
2023-03-20 00:20:53 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 杉下友麻 on 2023/03/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
