a8174d29a06ddcf8995b7ed9ac08748307ba8b49
U-ma-s <yuma.prodia4989@gmail.com>
2023-04-01 09:50:19 +0900

    delete template

--- Landmarks/Models/ModelData.swift
+++ Landmarks/Models/ModelData.swift
@@ -11,22 +11,33 @@ import Combine
 final class ModelData: ObservableObject {//観察対象のデータストアとなるオブジェクト(Observable Object?)？
     @Published var landmarks: [Landmark] = load("landmarkData.json")//observable objectのプロパティをpublishedに
     var hikes: [Hike] = load("hikeData.json")
+    
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured } // isFeaturedがtrueのランドマークのみを含む配列を作成
+    }
+    
+    var categories: [String:[Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: {$0.category.rawValue}
+        )
+    }
 }
 
 func load<T: Decodable>(_ filename: String) -> T {//<T: Decodable>はDecodableπロトコルに準拠したあらゆる型Tを型引数に取る．同じ型Tをreturn．
     let data: Data
-
+    
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
     else {
         fatalError("Couldn't find \(filename) in main bundle.")
     }
-
+    
     do {
         data = try Data(contentsOf: file)
     } catch {
         fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
     }
-
+    
     do {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
